bf1c245d181e14c564402bec9c60bf0a6e689148
Jelle Koomen <70689826+Jelleinholland@users.noreply.github.com>
2024-11-27 14:17:47 +0100

    Implemented Comments. About DTO and Account Endpoints.

--- src/main/java/hydroguard/api/mock/models/reocurringTask/ReoccurringTasksDTO.java
+++ src/main/java/hydroguard/api/mock/models/reocurringTask/ReoccurringTasksDTO.java
@@ -1,39 +1,13 @@
 package hydroguard.api.mock.models.reocurringTask;
-
+import hydroguard.api.mock.models.GetMoreDTO;
 import java.util.List;
 
-public class ReoccurringTasksDTO {
-    private List<ReoccurringTaskDTO> tasks;
-    private int total;
-    private int page;
-
-    public ReoccurringTasksDTO(List<ReoccurringTaskDTO> tasks, int total, int page) {
-        this.tasks = tasks;
-        this.total = total;
-        this.page = page;
+public class ReoccurringTasksDTO extends GetMoreDTO<ReoccurringTaskDTO> {
+    public ReoccurringTasksDTO() {
     }
 
-    public List<ReoccurringTaskDTO> getTasks() {
-        return tasks;
+    public ReoccurringTasksDTO(List<ReoccurringTaskDTO> data, int total, int currentPage) {
+        super(data, total, currentPage);
     }
 
-    public void setTasks(List<ReoccurringTaskDTO> tasks) {
-        this.tasks = tasks;
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
 }
--- src/main/java/hydroguard/api/mock/models/task/TasksDTO.java
+++ src/main/java/hydroguard/api/mock/models/task/TasksDTO.java
@@ -1,39 +1,15 @@
 package hydroguard.api.mock.models.task;
 
+import hydroguard.api.mock.models.GetMoreDTO;
+
 import java.util.List;
 
-public class TasksDTO {
-    private List<TaskDTO> tasks;
-    private int total;
-    private int page;
-
-    public TasksDTO(List<TaskDTO> tasks, int total, int page) {
-        this.tasks = tasks;
-        this.total = total;
-        this.page = page;
+public class TasksDTO extends GetMoreDTO<TaskDTO> {
+    public TasksDTO() {
     }
 
-    public List<TaskDTO> getTasks() {
-        return tasks;
+    public TasksDTO(List<TaskDTO> data, int total, int currentPage) {
+        super(data, total, currentPage);
     }
 
-    public void setTasks(List<TaskDTO> tasks) {
-        this.tasks = tasks;
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
 }
